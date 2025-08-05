f57568bd7aa661e20cc966cbcd528b2f089aa482
Drakalyebus <mishastelephonom@gmail.com>
2025-08-05 13:04:38 +0300

    rules fix

--- rules.js
+++ rules.js
@@ -1,71 +1,25 @@
-// export const rules = {
-//     "types": [0, 1],
-//     "kernels": {
-//         "moore": [
-//             [1, 1, 1],
-//             [1, 0, 1],
-//             [1, 1, 1]
-//         ]
-//     },
-//     "default": {
-//         "kernel": "moore"
-//     },
-//     "behavior": [
-//         {
-//             "from": [0],
-//             "condition": {
-//                 "value": [1],
-//                 "sign": "=",
-//                 "count": 3,
-//                 "kernel": "moore"
-//             },
-//             "elseTo": 0,
-//             "to": 1
-//         },
-//         {
-//             "from": [1],
-//             "condition": {
-//                 "or": [
-//                     {
-//                         "value": [1],
-//                         "sign": "<",
-//                         "count": 2,
-//                         "kernel": "moore"
-//                     },
-//                     {
-//                         "value": [1],
-//                         "sign": ">",
-//                         "count": 3,
-//                         "kernel": "moore"
-//                     }
-//                 ]
-//             },
-//             "elseTo": 1,
-//             "to": 0
-//         }
-//     ]
-// };
-
 export const rules = {
-    "types": [0, 1, 2],
+    "types": [0, 1],
     "kernels": {
-        "vonNeumann": [
-            [0, 1, 0],
+        "moore": [
+            [1, 1, 1],
             [1, 0, 1],
-            [0, 1, 0]
+            [1, 1, 1]
         ]
     },
     "default": {
-        "kernel": "vonNeumann"
+        "kernel": "moore"
     },
     "behavior": [
         {
             "from": [0],
             "condition": {
-                "expression": "random()",
-                "sign": "<",
-                "value": 0.15
+                "value": [1],
+                "sign": "=",
+                "count": 3,
+                "kernel": "moore"
             },
+            "elseTo": 0,
             "to": 1
         },
         {
@@ -73,22 +27,20 @@ export const rules = {
             "condition": {
                 "or": [
                     {
-                        "value": [2],
-                        "sign": ">",
-                        "count": 0,
-                        "kernel": "vonNeumann"
+                        "value": [1],
+                        "sign": "<",
+                        "count": 2,
+                        "kernel": "moore"
                     },
                     {
-                        "expression": "random()",
-                        "sign": "<",
-                        "value": 0
+                        "value": [1],
+                        "sign": ">",
+                        "count": 3,
+                        "kernel": "moore"
                     }
                 ]
             },
-            "to": 2
-        },
-        {
-            "from": [2],
+            "elseTo": 1,
             "to": 0
         }
     ]
